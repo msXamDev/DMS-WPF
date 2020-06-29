339183c088789b255602356aebea07814faed848
Mohammed Saquib <33442600+sqbsid@users.noreply.github.com>
2020-06-29 15:22:47 +0300

    new property added in model

--- MyScreens/Models/Login.cs
+++ MyScreens/Models/Login.cs
@@ -7,9 +7,10 @@ using System.Threading.Tasks;
 namespace MyScreens.Model
 {
     class Login
-    {
+    {     
         public int id { get; set; }
         public string username { get; set; }
         public string password { get; set; }
+        public DateTime date { get; set; }
     }
 }
